6096ef5b9da9e7a5df02472e2724bc07dc4217f7
Ibrahim635z <imohahm635z@g.educaand.es>
2025-09-29 22:41:06 +0200

    Monday 29/09 Correct Script Validation

--- scripts/Validacion.sh
+++ scripts/Validacion.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 programas=("git" "node" "npm" "curl" "jq")
-archivos=("../.env.example" "../.gitignore"  "../package.json")
+archivos=("C:\Users\chakr\Documents\semana01\.env.example" "C:\Users\chakr\Documents\semana01\.gitignore"  "C:\Users\chakr\Documents\semana01\package.json" "C:\Users\chakr\Documents\semana01\src" "C:\Users\chakr\Documents\semana01\src")
 
 for programa in "${programas[@]}"; do
-    # Comprueba si el programa existe en el sistema command busca el ejecutable
-    if command -v "$programa" &> /dev/null; then
-        echo "$programa está instalado."
+    # Comprueba si el programa existe en el sistema, command busca el ejecutable
+    if command -v "$programa" > /dev/null 2>&1; then
+        version=$($programa --version)
+        echo "$programa $version está instalado."
     else
         echo "$programa NO está instalado."
     fi
@@ -14,7 +15,7 @@ done
 
 
 for archivo in "${archivos[@]}"; do
-	# -e comprueba si es un archivo o archivo sin que importe el tipo
+	# -e comprueba si es un archivo o un directorio sin que importe el tipo
     if [ -e "$archivo" ]; then
         echo "El archivo '$archivo' existe."
     else
